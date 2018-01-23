fb7aac14696a50c304beaadd9d2833cdb8946f30
Ben Matare <ben.a.matare@gmail.com>
2018-01-23 04:45:08 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -75,17 +75,11 @@ def adding_matz
        :known_for => "Ruby",
        :languages => ["Lisp","C"]
      }
-     # add the following information to the top level of programmer_hash
-     # :yukihiro_matsumoto => {
-     #   :known_for => "Ruby",
-     #    :languages => ["LISP, C"]
-     # }
-
 return programmer_hash
-
 end
 
 def changing_alan
+  alans_new_info = "GUI"
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -101,10 +95,9 @@ def changing_alan
           :languages => ["C"]
         }
      }
-		#
+		programmer_hash[:alan_kay][:known_for] = alans_new_info
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-
-		 return programmer_hash
+     return programmer_hash
 end
 
 def adding_to_dennis
